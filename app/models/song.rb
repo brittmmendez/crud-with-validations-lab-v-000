6acc188b9bfedcd8b9e2baba871dbc13cedb5f8f
Brittany Morris <brittmmendez@gmail.com>
2018-02-15 12:42:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,14 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: { scope: [:release_year, :artist_name], message: "should happen once per year" }
   validates :released, inclusion: { in: %w(True False) }
 
-  with_options if: released==true do |song|
+  with_options if: :released? do |song|
    song.validates :release_year, presence: true
    admin.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
  end
+
+ def released?
+    released
+  end
 
   validates :artist_name, presence: true
 
